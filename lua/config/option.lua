1c8a2866dd63b9fef41497a243a02e68a8ae6d0e
mahiro2211 <dj4569103@gmail.com>
2026-02-18 10:57:36 +0800

    update

--- lua/config/option.lua
+++ lua/config/option.lua
@@ -2,3 +2,4 @@ vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.o.tabstop = 4
+vim.opt.cursorline = true
---
+++ lua/plugins/dap.lua
@@ -0,0 +1,10 @@
+return {
+	-- Source - https://stackoverflow.com/a/77575033
+	-- Posted by Francisco Gomes
+	-- Retrieved 2026-02-18, License - CC BY-SA 4.0
+
+	{
+		"mfussenegger/nvim-dap",
+		config = function() end,
+	},
+}
